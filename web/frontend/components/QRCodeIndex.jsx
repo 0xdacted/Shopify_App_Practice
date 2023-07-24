a07c0c772785faab61b0412364a5433ccb94b74a
Red <crabmarket56@gmail.com>
2023-07-24 16:40:51 +0900

    Add QR Code Index

--- web/frontend/components/QRCodeIndex.jsx
+++ web/frontend/components/QRCodeIndex.jsx
@@ -64,3 +64,95 @@ function SmallScreenCard({
     </UnstyledLink>
   );
 }
+
+export function QRCodeIndex({ QRCodes, loading })
+{
+  const navigate = useNavigate();
+  const isSmallScreen = useMedia("(max-width:640px)");
+  const smallScreenMarkup = QRCodes.map((QRCode) => (
+    <SmallScreenCard key={QRCode.id} navigate={navigate} {...QRCode} />
+  ));
+
+  const resourceName = {
+    singular: "QR code",
+    plural: "QR codes",
+  };
+
+  const rowMarkup = QRCodes.map(
+    ({ id, title, product, discountCode, scans, createdAt }, index) => {
+      const deletedProduct = product.title.includes("Deleted product");
+
+      return (
+        <IndexTable.Row
+          id={id}
+          key={id}
+          position={index}
+          onClick={() => {
+            navigate(`/qrcodes/${id}`);
+          }}
+        >
+          <IndexTable.Cell>
+            <Thumbnail
+              source={product?.images?.edges[0]?.node?.url || ImageMajor}
+              alt="placeholder"
+              color="base"
+              size="small"
+            />
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            <UnstyledLink data-primary-link url={`/qrcodes/${id}`}>
+              {truncate(title, 25)}
+            </UnstyledLink>
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            <Stack>
+              {deletedProduct && (
+                <Icon source={DiamondAlertMajor}
+                color="critical" />
+              )}
+              <TextStyle variation={deletedProduct ? "negative" : null}>
+                {truncate(product?.title, 25)}
+              </TextStyle>
+            </Stack>
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            {discountCode}
+          </IndexTable.Cell>
+          <IndexTable.Cell>
+            {dayjs(createdAt).format("MMMM D, YYYY")}
+          </IndexTable.Cell>
+          <IndexTable.Cell>{scans}
+          </IndexTable.Cell>
+        </IndexTable.Row>
+      );
+    }
+  );
+  return (
+    <Card>
+      {isSmallScreen ? (
+        smallScreenMarkup
+      )  :(
+        <IndexTable
+          resourceName={resourceName}
+          itemCount={QRCodes.length}
+          headings={[
+            { title: "Thumbnail", hidden: true },
+            { title: "Title" },
+            { title: "Product" },
+            { title: "Discount" },
+            { title: "Date created" },
+            {title: "Scans" },
+          ]}
+          selectable={false}
+          loading={loading}
+        >
+          {rowMarkup}
+        </IndexTable>
+      )}
+    </Card>
+  );
+}
+
+function truncate(str, n) {
+  return str.length > n ? str.substr(0, n - 1) + "…" : str;
+}
